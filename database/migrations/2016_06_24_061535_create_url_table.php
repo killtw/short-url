23b3d423de9daa3508ed5d5720d56018ffd13b64
Karl Li <killtw@gmail.com>
2016-06-24 17:13:59 +0800

    add url model and migration

---
+++ database/migrations/2016_06_24_061535_create_url_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUrlTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('urls', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('href');
+            $table->string('hash')->index()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('urls');
+    }
+}
